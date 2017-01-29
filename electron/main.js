6aef1282aa1e825c6090bc7d39505d651d9253ab
npezza93 <npezza93@gmail.com>
2017-01-29 11:53:59 -0500

    Get copy and paster working in electron

--- electron/main.js
+++ electron/main.js
@@ -21,6 +21,26 @@ function createWindow () {
     }
   });
 
+  var template = [{
+    label: "Eighty",
+    submenu: [
+      { label: "About Eighty", selector: "orderFrontStandardAboutPanel:" },
+      { type: "separator" },
+      { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+      label: "Edit",
+      submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
+  ];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
   // and load the index.html of the app.
   win.loadURL("https://eighty.herokuapp.com");
 
