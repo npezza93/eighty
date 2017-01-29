bfcf280d29e2706913e808becff7fc3181d538be
npezza93 <npezza93@gmail.com>
2017-01-29 11:47:28 -0500

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
+
   devise_for :users
   resources :notes, except: :new
   resources :images, only: :create
+  resource :about, only: :show, controller: :about
 
   root to: "notes#index"
 end
---
+++ test/controllers/about_controller_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+require "test_helper"
+
+class AboutControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get about_url
+    assert_response :success
+  end
+end
