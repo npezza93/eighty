3895783cf89dced3cf490b5cb7b6379929492106
Nick Pezza <npezza93@gmail.com>
2019-05-19 11:02:23 -0400

    Fix tests

--- test/models/html_with_pygments_test.rb
+++ test/models/html_with_pygments_test.rb
@@ -8,12 +8,13 @@ class HTMLWithPygmentsTest < ActiveSupport::TestCase
   test "an unordered task list can be created from markdown" do
     note = notes(:unordered_task_list)
     markdown = markdown(note)
-    assert markdown ==
-      "<ul class=\"task-list\"><li class=\"task-list-item\"><input "\
-      "type=\"checkbox\" class=\"task-list-item-checkbox\" disabled><span>"\
-      "Task 1</span></li><li class=\"task-list-item\"><input type=\"checkbox\""\
-      " class=\"task-list-item-checkbox\" disabled>"\
-      "<span>Task 2</span></li></ul>"
+    assert_equal markdown,
+                 "<ul class=\"task-list\"><li class=\"task-list-item\"><input "\
+                 "type=\"checkbox\" class=\"task-list-item-checkbox\""\
+                 " disabled=\"disabled\"><span>"\
+                 "Task 1</span></li><li class=\"task-list-item\"><input type=\"checkbox\""\
+                 " class=\"task-list-item-checkbox\" disabled=\"disabled\">"\
+                 "<span>Task 2</span></li></ul>"
   end
 
   test "an ordered task list can be created from markdown" do
@@ -23,7 +24,7 @@ class HTMLWithPygmentsTest < ActiveSupport::TestCase
     assert markdown =~ /<ol class=\"task-list\">/
     assert markdown.include?(
       "<li class=\"task-list-item\"><input type=\"checkbox\" "\
-      "class=\"task-list-item-checkbox\" disabled>"\
+      "class=\"task-list-item-checkbox\" disabled=\"disabled\">"\
       "<span>Task 1</span></li>"
     )
   end
@@ -32,17 +33,17 @@ class HTMLWithPygmentsTest < ActiveSupport::TestCase
     note = notes(:completed_unordered_task_list)
     markdown = markdown(note)
 
-    assert markdown ==
-      "<ul class=\"task-list\"><li class=\"task-list-item checked\"><input "\
-      "type=\"checkbox\" class=\"task-list-item-checkbox\" "\
-      "disabled checked><span>Task 1</span>"\
-      "</li></ul>"
+    assert_equal markdown,
+                 "<ul class=\"task-list\"><li class=\"task-list-item checked\"><input "\
+                 "type=\"checkbox\" class=\"task-list-item-checkbox\" "\
+                 "disabled=\"disabled\" checked=\"checked\"><span>Task 1</span>"\
+                 "</li></ul>"
   end
 
   test "regular unordered lists are generated" do
     note = notes(:regular_unordered_list)
     markdown = markdown(note)
 
-    assert markdown == "<ul><li>Item 1\n</li></ul>"
+    assert_equal markdown, "<ul><li>Item 1\n</li></ul>"
   end
 end
